96c97a62c27aebaa35b575d2fc31b1d9a6d9b075
Nate2066 <fredericknat1522@gmail.com>
2024-10-31 06:53:16 -0600

    cambios 02 nate

--- AplicacionViajes/PantallaDeVuelo.Designer.cs
+++ AplicacionViajes/PantallaDeVuelo.Designer.cs
@@ -82,10 +82,12 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
             this.ClientSize = new System.Drawing.Size(686, 450);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.bttProgramarViaje);
             this.Controls.Add(this.dataTableroViajesDisponibles);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
             this.Name = "PantallaDeVuelo";
             this.Text = "PantallaDeVuelo";
             ((System.ComponentModel.ISupportInitialize)(this.dataTableroViajesDisponibles)).EndInit();
--- AplicacionViajes/PantallaDeVuelo.cs
+++ AplicacionViajes/PantallaDeVuelo.cs
@@ -13,16 +13,18 @@ namespace AplicacionViajes
     public partial class PantallaDeVuelo : Form
     {
         public ViajesDisponibles classViajesDisponibles;
+        public ProgramarViaje formProgramarViaje;
         public PantallaDeVuelo()
         {
             InitializeComponent();
             classViajesDisponibles = new ViajesDisponibles(dataTableroViajesDisponibles);
             classViajesDisponibles._ActualizarData();
+            formProgramarViaje = new ProgramarViaje();
         }
 
         private void bttProgramarViaje_Click(object sender, EventArgs e)
         {
-
+            formProgramarViaje.Show();
         }
     }
 }
